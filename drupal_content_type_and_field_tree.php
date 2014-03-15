ef34fff88a0248c5d74b0dacd1c63a983f9d0efa
Peter Arato <it.arato@gmail.com>
2014-03-15 11:22:02 +0000

    Clean code.

--- drupal_content_type_and_field_tree.php
+++ drupal_content_type_and_field_tree.php
@@ -1,32 +1,51 @@
 <?php
 
-function foobar() {
-  $info = '<map version="0.9.0">' . "\n" . '<node TEXT="Content types">';
+class Drupal7Mindmap {
 
-  $entity_type = 'node';
-  $entities_info = entity_get_info($entity_type);
-  foreach ($entities_info['bundles'] as $bundle => $entity_info) {
-    $info .= get_fields($entity_type, $bundle) . "\n";
+  public static function generate() {
+    $info = '<map version="0.9.0">' . "\n" . '  <node TEXT="Drupal">' . "\n";
+
+    $entity_types = entity_get_info();
+    $position = 'right';
+    foreach ($entity_types as $type => $entities_info) {
+      $info .= '    <node TEXT="' . urlencode($type) . '" POSITION="' . $position . '">' . "\n";
+      $bundle_info = array();
+      foreach ($entities_info['bundles'] as $bundle => $entity_info) {
+        $info .= self::getFields($type, $bundle, '      ') . "\n";
+      }
+      $info .= "    </node>\n";
+      $position = $position == 'right' ? 'left' : 'right';
+    }
+
+    $info .= '  </node>' . "\n" . '</map>';
+
+    drupal_add_http_header('Content-Type', 'application/x-freemind');
+    drupal_add_http_header('Content-Disposition', 'attachment; filename=drupal_architecture.mm;');
+
+    echo $info;
+
+    drupal_exit();
   }
 
-  $info .= '</node>' . "\n" . '</map>';
+  private static function getFields($entity_type, $bundle, $indentation = '') {
+    $info = $indentation . '<node TEXT="' . urlencode($bundle) . '">' . "\n";
 
-  return $info;
-}
-
-function get_fields($entity_type, $bundle) {
-  $info = '<node TEXT="' . $bundle . '">' . "\n";
-
-  $field_instances = field_info_instances($entity_type, $bundle);
-  foreach ($field_instances as $field_name => $field_instance) {
-    $field_info = field_info_field($field_name);
-    if ($field_info['type'] == 'field_collection') {
-      $info .= get_fields('field_collection_item', $field_name) . "\n";
+    $field_instances = field_info_instances($entity_type, $bundle);
+    foreach ($field_instances as $field_name => $field_instance) {
+      $field_info = field_info_field($field_name);
+      if ($field_info['type'] == 'field_collection') {
+        $info .= self::getFields('field_collection_item', $field_name, $indentation . '  ') . "\n";
+      }
+      else {
+        $info .= $indentation . '  <node TEXT="' . urlencode($field_name) . '"/>' . "\n";
+      }
     }
-    else {
-      $info .= '<node TEXT="' . $field_instance['label'] . '" />' . "\n";
-    }
+
+    $info .=  $indentation . '</node>';
+
+    return $info;
   }
 
-  return $info . '</node>' . "\n";
 }
+
+Drupal7Mindmap::generate();
